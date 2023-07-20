153f2b7695b855916d2e51962aad893a17e84e80
Luca <root@gituser.de>
2023-07-20 22:18:08 +0200

    add auto update

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      4
+// @version      5
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
@@ -10,13 +10,19 @@
 // @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
 // ==/UserScript==
 
-let url = "https://place.army/overlay_target.png"
+var overlayImage = null;
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         const canvasContainer = document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].getElementsByTagName("garlic-bread-canvas")[0].shadowRoot.children[0];
-        const overlayImage = document.createElement("img");
-        overlayImage.src = url;
-        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 100opx;height: 1000px;`;
+        overlayImage = document.createElement("img");
+        updateImage();
+        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;`;
         canvasContainer.appendChild(overlayImage);
     }, false);
 }
+
+function updateImage() {
+    overlayImage.src = "https://place.army/overlay_target.png?" + Date.now()
+}
+
+setInterval(function () {overlayImage.src = "https://place.army/overlay_target.png?" + Date.now()}, 30000);
