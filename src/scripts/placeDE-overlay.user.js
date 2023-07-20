20d5f3d3baae995a498617e1089482417d253612
Kronnox <cn@gituser.de>
2023-07-20 20:26:39 +0200

    update author

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      4
 // @description  try to take over the canvas!
-// @author       mp1404
+// @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @updateURL    https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
