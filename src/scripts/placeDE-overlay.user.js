1485852630289cbb35e34c40148b7e166d47783f
TheCataliasTNT2k <44349750+TheCataliasTNT2k@users.noreply.github.com>
2023-07-21 18:11:07 +0200

    Update placeDE-overlay.user.js

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/placeDE Template
 // @namespace    http://tampermonkey.net/
-// @version      7
+// @version      8
 // @description  try to take over the canvas!
 // @author       placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
