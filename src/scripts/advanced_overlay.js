10905d89b185bbf2d535ca5bf9d1fb4c77ac88fc
TheCataliasTNT2k <44349750+TheCataliasTNT2k@users.noreply.github.com>
2023-07-22 10:21:51 +0200

    Update advanced_overlay.js

--- src/scripts/advanced_overlay.js
+++ src/scripts/advanced_overlay.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/place 2023 Canada Overlay with German tiles
 // @namespace    http://tampermonkey.net/
-// @version      0.8.4
+// @version      0.8.5
 // @description  Script that adds a button to toggle an hardcoded image shown in the 2023's r/place canvas
 // @author       max-was-here
 // @match        https://garlic-bread.reddit.com/embed*
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-let width = "1500px";
+let width = "2000px";
 let height = "1000px";
 var button = null;
 
