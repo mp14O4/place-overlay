9d745b1c88606b9548e2833d0bf0bd76f2b92793
Luca <root@gituser.de>
2023-07-24 12:22:03 +0200

    bump version

--- src/scripts/advanced_overlay.user.js
+++ src/scripts/advanced_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/place 2023 Canada Overlay with German tiles
 // @namespace    http://tampermonkey.net/
-// @version      0.11
+// @version      0.12
 // @description  Script that adds a button to toggle an hardcoded image shown in the 2023's r/place canvas
 // @author       max-was-here and placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed*
